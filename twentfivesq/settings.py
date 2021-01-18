e29c11eb0ac80b2d97c8f45a6ee5326d29eb2e3c
Steve M Silva <silvasm516@aol.com>
2021-01-18 12:30:39 -0800

    Update settings.py

--- twentfivesq/settings.py
+++ twentfivesq/settings.py
@@ -22,8 +22,8 @@ SECRET_KEY ='Fl53290#$6503484818#$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.0', '5640', 'heroku', '162.241.224.107', 'host=fast-lowlands-84252.herokuapp.com'
-]
+ALLOWED_HOSTS = ['5640', 'heroku', '162.241.224.107', 'host=fast-lowlands-84252.herokuapp.com']
+
 
 
 # Application definition
