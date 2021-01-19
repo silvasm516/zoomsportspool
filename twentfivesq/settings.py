6f273abba49005830833dfc0c9d46877ec61e1ee
Steve M Silva <silvasm516@aol.com>
2021-01-18 18:08:32 -0800

    Update settings.py

--- twentfivesq/settings.py
+++ twentfivesq/settings.py
@@ -144,3 +144,5 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 ##STATICFILES_DIRS = (
 ##    os.path.join(PROJECT_ROOT,'twentfivesq/twentfivesq/', 'static'),
 ##)
+import dj_database_url
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
